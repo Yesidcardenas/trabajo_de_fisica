a5c6a2cac9deb585d28b5a0b4493366f486c3d48
Yesidcardenas <cardenas09941994@gmail.com>
2022-03-31 20:53:49 -0500

    YESID_31-03-2022
    EJERCICIO DE ARRAY PARA NOMBRES Y APELLIDOS

---
+++ ejercicio array.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("hola mundo")
+
+nombres = []
+apellidos = []
+
+  for X in range(3):
+      
+  nom = input("por favor ingrese los nombres:")
+  nombres.apppend(nom)
+
+
+  apellido1 = input("por favor ingrese primer apellido:")    
+  apellido2 = input("por favor ingrese segundo apellido:")
+  
+
+
+
+
+
+
+
